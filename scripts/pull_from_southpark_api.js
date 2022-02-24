781792df85eb0f6784745b513458efda320c03f8
jeffj6123 <jeffrey.jarry@gmail.com>
2022-02-24 11:22:45 -0800

    added new images.

--- scripts/pull_from_southpark_api.js
+++ scripts/pull_from_southpark_api.js
@@ -3,51 +3,22 @@ const path = require('path');
 const axios = require('axios');
 const fsP = require('fs/promises')
 
-// // https://spapi.dev/api/episodes/1
+const getEpisodes = () => {
+    let episodes = [];
+    const loop = 32;
 
-// const episodes = [];
-
-// for (let i = 1; i < 304; i++) {
-//     try {
-//         setTimeout(() => {
-//             axios.get(`https://spapi.dev/api/episodes/${i}`).then(body => {
-//                 const data = JSON.stringify(body.data);
-
-//                 try {
-//                     episodes.push(body.data);
-//                     fs.writeFile(path.join(__dirname, 'episodes', `${body.data.season}-${body.data.episode}.json`), data, (err) => {
-//                         if (err) {
-//                             throw err;
-//                         }
-//                     });
-//                 } catch (e) {
-//                     console.log(e)
-//                     console.log("could not save " + i)
-//                 }
-//             });
-//         }, 1000 * i)
-
-//     } catch (e) {
-//         console.log(" could not load " + i)
-//     }
-// }
-
-const getchars = () => {
-    characters = [];
-
-    for (let i = 1; i < 22; i++) {
+    for (let i = 1; i < loop; i++) {
         try {
             setTimeout(() => {
-                axios.get(`https://spapi.dev/api/characters?page=${i}`).then(body => {
-                    // const data = JSON.stringify(body.data);
+                axios.get(`https://spapi.dev/api/episodes?page=${i}`).then(body => {
 
-                    characters = characters.concat(body.data.data);
+                    episodes = episodes.concat(body.data.data);
 
-                    if (i === 21) {
-                        fsP.writeFile(path.join(__dirname, `characters.json`), JSON.stringify(characters))
+                    if (i === (loop - 1)) {
+                        fsP.writeFile(path.join(__dirname, `episodes2.json`), JSON.stringify(episodes.sort((a,b) => a.id - b.id)))
                     }
                 })
-            }, 500 * i)
+            }, 100 * i)
 
         } catch (e) {
             console.log(" could not load " + i)
@@ -55,20 +26,29 @@ const getchars = () => {
     }
 }
 
-getchars();
+const getCharacters = () => {
+    let episodes = [];
+    const loop = 23;
 
-// const f = async () => {
-//     episodes =[];
-//     fs.readdirSync('./episodes').forEach(file => {
-//         if(file.includes('.json')) {
-//             const data = fs.readFileSync('./episodes/' + file);
-//             episodes.push(JSON.parse(data));
-//             console.log(file);
-//         }
-//       });    
+    for (let i = 1; i < loop; i++) {
+        try {
+            setTimeout(() => {
+                axios.get(`https://spapi.dev/api/characters?page=${i}`).then(body => {
+                    // const data = JSON.stringify(body.data);
 
-//       console.log(episodes)
-//       fs.writeFileSync("all_episodes.json", JSON.stringify(episodes), null, 4)
-//     }
+                    episodes = episodes.concat(body.data.data);
 
-// f();
+                    if (i === (loop - 1)) {
+                        fsP.writeFile(path.join(__dirname, `characters2.json`), JSON.stringify(episodes.sort((a,b) => a.id - b.id)))
+                    }
+                })
+            }, 100 * i)
+
+        } catch (e) {
+            console.log(" could not load " + i)
+        }
+    }
+}
+
+getCharacters()
+// getEpisodes();
